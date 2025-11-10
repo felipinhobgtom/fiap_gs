d3dcbfd48f64c8e964e890d933a9a2e923e067b4
= <=>
2025-11-10 09:45:51 -0300

    feat: exerc4

---
+++ exercs/ex4rm568371.py
@@ -0,0 +1,30 @@
+import socket as s, sys
+
+ALVO = '10.3.40.100'
+PORTA = 5000
+
+try:
+    con = s.socket(s.AF_INET,s.SOCK_STREAM)
+    con.settimeout(10)
+    con.connect((ALVO,PORTA))
+
+    resp = con.recv(4096)
+
+    # con.sendall(().encode())    
+        
+    #recuperando a mensagem enviada
+    print("Resposta:", resp.decode(errors="ignore").rstrip(), flush=True)
+    resp_split = resp.decode(errors="ignore").split(': ')
+
+    pergunta = resp_split[1].split(' e ')
+    # print(pergunta)
+
+    soma = str(int(pergunta[0]) + int(pergunta[1]))
+    print(f'Soma dos números: {soma}')
+
+    con.send(soma.encode())
+    print(con.recv(4096))
+
+    con.close()
+except Exception as e:
+    print(e)
