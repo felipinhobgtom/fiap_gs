10a598feedd3eb2386c8ceeae9a2f01480369d80
= <=>
2025-11-10 10:00:23 -0300

    feat: exerc5

---
+++ exercs/ex5rm568371.py
@@ -0,0 +1,20 @@
+import requests, json
+
+API_1 = 'https://api.ipify.org/'
+
+response = requests.get(API_1)
+meu_ip = response.text
+
+API_2 = f'http://ip-api.com/json/{meu_ip}'
+
+res = requests.get(API_2).json()
+# print(res)
+
+obj = {
+    'Meu IP': meu_ip,
+    'País': res['country'],
+    'Cidade': res['regionName'],
+    'Latitude/Longitude': f'{res['lat']}/{res['lon']}'
+}
+
+print(obj)
