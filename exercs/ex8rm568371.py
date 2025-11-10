f588ab51dc8ead17e7dcf366e28126e59719f0ed
= <=>
2025-11-10 10:48:14 -0300

    exerc8

---
+++ exercs/ex8rm568371.py
@@ -0,0 +1,17 @@
+import requests
+
+id = 4
+API_4 = f"https://jsonplaceholder.typicode.com/users/{id}"
+res = requests.get(API_4)
+
+payload = res.json()
+
+print(f'Nome: {payload['name']}')
+print(f'E-mail: {payload['email']}')
+
+res_posts = requests.get('https://jsonplaceholder.typicode.com/posts')
+posts_payload = res_posts.json()
+
+for post in posts_payload:
+    if post['userId'] == id:
+        print(f'Titulo: {post['title']}')
