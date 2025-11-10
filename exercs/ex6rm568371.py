1aba3f579e02e5e421801ab9a10e7dbe7e843ff9
= <=>
2025-11-10 10:21:12 -0300

    feat: exerc6

---
+++ exercs/ex6rm568371.py
@@ -0,0 +1,13 @@
+import requests
+
+API_3 = "https://httpbin.org/get"
+res = requests.get(API_3, headers={
+    'User-Agent': 'nao'
+})
+
+payload = res.json()
+headers = payload['headers']
+
+print(f'Headers: {headers}')
+print(f'User agent: {headers['User-Agent']}')
+print(f'Status code: {res.status_code}')
