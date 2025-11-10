00b8f69d891553d3245087085808016b1faf74e8
= <=>
2025-11-10 08:53:06 -0300

    feat: exerc2

---
+++ exercs/ex2rm568371.py
@@ -0,0 +1,16 @@
+import os
+
+while True:
+    site = input("Digite um site: ")
+    ping = os.popen(f"ping {site} -n 1")
+    
+    # isso aq ta uma gambiarra nojenta jesus
+    ponga = str(ping.read()).split(' ')
+    # print(ponga)
+    
+    if ponga[0] == '\nPinging':
+        ip_string = ponga[12].split('\n')[0]
+        ttl_string = ponga[9].split(':')[0]
+        print(f"Host respondeu! IP: {ip_string}, TTL: {ttl_string}")
+    else:
+        print('Host não respondeu.')
